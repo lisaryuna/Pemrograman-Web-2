6451237460472ded9ce0caf05618a81ad3cc395b
Lisa <fa037024@gmail.com>
2026-05-28 11:09:44 +0800

    feat: create borrowing list table view

---
+++ Modul5/PRAK501/views/Peminjaman.php
@@ -0,0 +1,46 @@
+<?php
+require_once '../models/Model.php';
+$peminjaman = getPeminjaman();
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Kelola Peminjaman - Perpustakaan Lisa</title>
+    </head>
+    <body>
+        <center>
+            <h2>Kelola Peminjaman</h2>
+
+            <a href="Index.php">Kembali ke Beranda</a><br><br>
+            <a href="FormPeminjaman.php">Tambah Peminjaman</a><br><br>
+
+            <table border="1" cellpadding="10" cellspacing="0">
+                <tr>
+                    <th>ID Peminjaman</th>
+                    <th>Nama Member</th>
+                    <th>Judul Buku</th>
+                    <th>Tanggal Pinjam</th>
+                    <th>Tanggal Kembali</th>
+                    <th>Opsi</th>
+                </tr>
+
+                <?php foreach ($peminjaman as $p) : ?>
+                <tr>
+                    <td><?= $p['id_peminjaman']; ?></td>
+                    <td><?= $p['nama_member']; ?></td>
+                    <td><?= $p['judul_buku']; ?></td>
+                    <td><?= $p['tgl_pinjam']; ?></td>
+                    <td><?= $p['tgl_kembali']; ?></td>
+                    <td>
+                        <a href="FormPeminjaman.php?id=<?= $p['id_peminjaman']; ?>">Edit</a> | 
+                        <a href="../controllers/PeminjamanController.php?action=hapus&id_peminjaman=<?= $p['id_peminjaman']; ?>" onclick="return confirm('Yakin ingin menghapus data peminjaman ini?')">Hapus</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        </center>
+    </body>
+</html>
