2777a7245dce3c144a5bad93a5dfebedbb846fdc
Lisa <fa037024@gmail.com>
2026-05-28 11:23:24 +0800

    style: apply card layout and water lily color palette

--- Modul5/PRAK501/views/Index.php
+++ Modul5/PRAK501/views/Index.php
@@ -4,15 +4,25 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Perpustakaan Lisa</title>
+        <link rel="stylesheet" href="../assets/css/style.css">
     </head>
 
     <body>
-        <center>
-            <h1>Selamat Datang di Perpustakaan Lisa</h1>
-            <p>Silakan pilih menu di bawah ini untuk mengelola data buku dan anggota perpustakaan.</p>
-            <a href="Buku.php">Kelola Buku</a><br>
-            <a href="Member.php">Kelola Anggota</a>
-            <a href="Peminjaman.php">Kelola Peminjaman</a>
-        </center>
+        <div class="home-container">
+            <img src="https://images.unsplash.com/photo-1463320726281-696a485928c7?q=80&w=800&auto=format&fit=crop" alt="Ilustrasi Buku dan Alam" class="home-image">
+            
+            <h1>Perpustakaan Lisa</h1>
+            <p class="home-subtitle">
+                Selamat datang! Silakan pilih menu di bawah ini untuk mulai mengelola sistem perpustakaan.
+            </p>
+
+            <div class="menu-container">
+                <a href="Buku.php"><button>Kelola Data Buku</button></a>
+                <a href="Member.php"><button>Kelola Data Anggota</button></a>
+                <a href="Peminjaman.php"><button>Kelola Peminjaman</button></a>
+            </div>
+        </div>
+
+        <script src="../assets/js/script.js"></script>
     </body>
 </html>
