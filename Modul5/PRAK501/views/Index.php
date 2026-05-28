6c172a933337f06cdff371b9bd6365b7c9fe6f20
Lisa <fa037024@gmail.com>
2026-05-28 11:08:28 +0800

    feat: create landing page layout

---
+++ Modul5/PRAK501/views/Index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Perpustakaan Lisa</title>
+    </head>
+
+    <body>
+        <center>
+            <h1>Selamat Datang di Perpustakaan Lisa</h1>
+            <p>Silakan pilih menu di bawah ini untuk mengelola data buku dan anggota perpustakaan.</p>
+            <a href="Buku.php">Kelola Buku</a><br>
+            <a href="Member.php">Kelola Anggota</a>
+            <a href="Peminjaman.php">Kelola Peminjaman</a>
+        </center>
+    </body>
+</html>
