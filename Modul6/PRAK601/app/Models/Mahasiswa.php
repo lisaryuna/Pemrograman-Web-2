c2a364e549048c3a22e90192f7bd4ce344035831
Lisa <fa037024@gmail.com>
2026-05-30 20:38:49 +0800

    feat: add static profile data to Mahasiswa model

--- Modul6/PRAK601/app/Models/Mahasiswa.php
+++ Modul6/PRAK601/app/Models/Mahasiswa.php
@@ -2,31 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class Mahasiswa 
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public static function getProfile()
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'nama' => 'Noor Khalisa',
+            'nim' => '2410817220012',
+            'prodi' => 'Teknologi Informasi',
+            'fakultas' => 'Teknik',
+            'universitas' => 'Universitas Lambung Mangkurat',
+            'skill' => 'Java, Kotlin, PHP, HTML, CSS',
+            'foto' => 'https://source.unsplash.com/random/300x300/?face',
         ];
     }
 }
