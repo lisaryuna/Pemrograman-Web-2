5b3e5a455496f15cb021aa0550078a9907e2849b
Lisa <fa037024@gmail.com>
2026-05-30 20:38:36 +0800

    feat: configure Pengalaman model for database table

---
+++ Modul6/PRAK601/app/Models/Pengalaman.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pengalaman extends Model
+{
+    protected $table = 'pengalaman';
+
+    protected $guarded = [];
+}
