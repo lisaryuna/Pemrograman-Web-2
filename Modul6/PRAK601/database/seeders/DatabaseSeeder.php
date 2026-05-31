6e85753ff8f90477709ba3e5dadfc82f78d003b8
Lisa <fa037024@gmail.com>
2026-05-31 12:32:29 +0800

    git commit -m "chore: remove unused comments from DatabaseSeeder"

--- Modul6/PRAK601/database/seeders/DatabaseSeeder.php
+++ Modul6/PRAK601/database/seeders/DatabaseSeeder.php
@@ -8,14 +8,8 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $this->call([
             PengalamanSeeder::class,
         ]);
