d734e6c6608eb8c437cb0a30e013ed7ded9ec215
Lisa <fa037024@gmail.com>
2026-06-11 21:17:12 +0800

    feat: setup Buku model with fillable properties and foreign keys

---
+++ Modul7/PRAK701/app/Models/Buku.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Buku extends Model
+{
+    protected $table ='buku';
+    protected $primaryKey = 'buku_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'kategori_id',
+        'judul',
+        'penulis',
+        'penerbit',
+        'tahun_terbit'
+    ];
+
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class, 'kategori_id', 'kategori_id');
+    }
+}
